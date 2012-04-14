07579f80166003a181eed888b6a1a18800e9990d
Russell <russell.mcclellan@gmail.com>
2012-04-14 16:27:45 -0400

    basic DB stuff is kinda working but untested.

--- src/db/index.js
+++ src/db/index.js
@@ -4,12 +4,114 @@
  *
  */
 
-var mongoose = require('mongoose');
 
+// define local variables up in here.
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    inited = false;
+
+
+// initialization
 module.exports = function()
 {
+    if(inited) return;
+
+    inited = true;
+        
     mongoose.connect('mongodb://localhost/remnants_tag');
-    mongoose.connection.on('open', function() {
-        console.log('OPENED DATABASE!!!');
+
+    ref = function(table) {
+        return {type: Schema.ObjectId, ref: table};
+    };
+
+    var PlayerSchema = new Schema({
+        name: String,
+        faction: ref('Faction'),
+	    inventory: [ref('Item')],
+	    stats: {
+		    Health: Number,
+            Manliness: Number,
+            Womanliness: Number
+	    }
     });
+    
+    var FactionSchema = new Schema({
+        name: String,
+    });
+
+    var ItemSchema = new Schema({
+	    name: String,
+	    type: {type: String, enum: [
+            "armor",
+            "weapon",
+            "ammo",
+            "onetime",
+            "quest"
+        ]},
+	    buffs: [{
+		    stat: String,
+		    buff: Number
+        }]
+    });
+
+    var Player = mongoose.model('Player', PlayerSchema);
+    var Item = mongoose.model('Item', ItemSchema);
+    var Faction = mongoose.model('Faction', FactionSchema);
+
+    var AddModel = function(model) {
+        return function(obj, callback) {
+            modeledObject = new model(obj);
+            modeledObject.save(function(err){
+                callback(err, modeledObject);
+            });
+        };
+    };
+    
+    var GetModel = function(model) {
+        return function(id, callback) {
+            model.findById(id, callback);
+        };
+    };
+
+    var DeleteModel = function(model) {
+        return function(id, callback) {
+            model.findById(id, function(err, obj){
+                if(!err)
+                    obj.remove(callback);
+                else
+                    callback(err);
+            });
+        };
+    };
+
+    var UpdateModel = function(model) {
+        return function(id, obj, callback) {
+            options = {};
+            model.updateById(id, obj, options, callback);
+        };
+    };
+
+    // now add all the methods.
+    var Models = {
+        'Player' : Player,
+        'Faction' : Faction,
+        'Item' : Item
+    };
+
+    for(model in Models) if(Models.hasOwnProperty(model)) {
+        console.log('creating methods for ' + model);
+        var AddMethod = function(prefix, func) {
+            console.log('Adding ' + prefix + model);
+            module.exports[prefix + model] = func(Models[model]);
+        };
+        var Methods = {
+            'Add' : AddModel,
+            'Get' : GetModel,
+            'Update' : UpdateModel,
+            'Delete' : DeleteModel
+        };
+        
+        for(method in Methods) if(Methods.hasOwnProperty(method))
+            AddMethod(method, Methods[method]);
+    }
 };
--- src/web/index.js
+++ src/web/index.js
@@ -5,15 +5,32 @@
  */
 
 var config = require(__dirname + '/../config'),
-	express = require('express');
+	express = require('express'),
+    db = require(__dirname + '/../db');
 
 
 module.exports = function() {
 	var app = express.createServer();
 
+    db();
+
 	app.get('/', function(req, res){
-		res.send('This was a triumph');
+        // db.AddFaction({name : 'faketion'}, function(err, fact) {
+        //     db.AddPlayer({
+        //         name : 'Namey',
+        //         faction : fact,
+        //         inventory : [],
+        //         stats : {
+        //             Health : 0,
+        //             Manliness: 1, 
+        //             Womanliness: 2
+        //         }}, function(err, player) {
+        //             res.send(player)
+        //     });
+        // });
+        res.send('poop!');
 	});
+
 
 	app.listen(config.port);
 	console.log('listening on port ' + config.port);

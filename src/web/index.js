e18e0777fe0ec9bcda6adc8db7429c1be40b8597
Chris Vickery <chrisinajar@gmail.com>
2012-04-14 18:32:02 +0100

    Basic admin/api/player routing and a 404 handler

--- src/web/index.js
+++ src/web/index.js
@@ -11,8 +11,38 @@ var config = require(__dirname + '/../config'),
 module.exports = function() {
 	var app = express.createServer();
 
-	app.get('/', function(req, res){
-		res.send('This was a triumph');
+	app.use(express.logger());
+	app.use(express.bodyParser());
+	//app.use(express.router());
+
+	// Set up error handlers
+	app.configure(function(){
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	});
+
+	// Root fetcher thing
+	app.get('/', function(req, res, next) {
+		res.redirect('/admin/');
+	});
+	
+	// admin bitch!
+	app.get('/admin/*?', function(req, res, next) {
+		res.send('This was an administrative triumph');
+	});
+	
+	// api bitch!
+	app.get('/api/*?', function(req, res, next) {
+		res.send('This was an api triumph');
+	});
+	
+	// player bitzh!
+	app.get('/player/*?', function(req, res, next) {
+		res.send('This was a player triumph');
+	});
+	
+	// 404!
+	app.get('/*', function(req, res, next) {
+		res.redirect('http://en.wikipedia.org/wiki/HTTP_404');
 	});
 
 	app.listen(config.port);

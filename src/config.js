9df96da04e2ab3854929a2b20a1ee7fa0bbb6f5c
Russell <russell.mcclellan@gmail.com>
2012-04-14 12:46:47 -0400

    port is now configurable

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@ module.exports = {
 	database: {
 		address: 'localhost',
 		port: 27017
-	}
+	},
+    port : 8080
 };
 
 
--- src/web/index.js
+++ src/web/index.js
@@ -5,12 +5,15 @@
  */
 
 var CornerCut = require('CornerCut');
+var config = require(__dirname + '/../config');
+
 
 module.exports = function() {
 	var cornercut = new CornerCut;
 
 	cornercut.add(require('./routes')());
 
-	cornercut.createServer(8080);
+	cornercut.createServer(config.port);
+    console.log('listening on port ' + config.port);
 };
 
